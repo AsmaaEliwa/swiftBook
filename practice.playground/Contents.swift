48216b971c5b5cb4a8d06942a7a1f2d827094c3b
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-10-01 18:15:12 -0400

    Arrays and Collections done

--- practice.playground/Contents.swift
+++ practice.playground/Contents.swift
@@ -15,6 +15,8 @@ func factorial(_ num: Int)->Int{
 print(factorial(4))
 
 
+
+
 //
 //Create a function that reverses a given string without using the reversed() method.
 func reverse(str:String)->String{
@@ -28,6 +30,11 @@ func reverse(str:String)->String{
     return newStr
 }
 print(reverse(str: "asmaa"))
+
+
+
+
+
 //
 //Implement a function to check if a string is a palindrome (reads the same forwards and backwards).
 
@@ -35,3 +42,61 @@ func ispalindrome(_ str: String)->Bool{
     return str == reverse(str: str)
 }
 ispalindrome("aca")
+
+
+
+
+
+
+
+//Arrays and Collections:
+//
+//Find the sum of all even numbers in an array of integers.
+
+func evenSum(arr: [Int])->Int{
+    var sum = 0
+    for i in arr {
+        if i % 2 == 0{
+            sum+=i
+        }
+    }
+    return sum
+}
+evenSum(arr: [1,2,3,4,5])
+
+
+
+//
+//Write a function to remove duplicates from an array while preserving the order of elements.
+func noduplicates(arr: [Int])->[Int]{
+    var newArr = [Int]()
+    for i  in arr{
+        if !newArr.contains(i){
+            newArr.append(i)
+        }
+    }
+    return newArr
+}
+noduplicates(arr: [1,2,3,1,2,3,4,5])
+
+
+
+
+
+
+
+//
+//Implement a function to rotate the elements of an array to the left by a specified number of positions.
+func rotate(arr: [Int] , k:Int)->Any{
+    return arr[k...] + arr[..<k]
+}
+
+print(rotate(arr: [1,2,3,4,5,6,7], k:3))
+
+
+
+
+
+
+
+
